9556dccd6709a6110f088abfa09774ddc6a7ec57
Ramdan <dadanisme@upi.edu>
2025-04-09 08:19:44 +0800

    Initial Commit

---
+++ C1-DatingApp/C1_DatingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  C1_DatingAppApp.swift
+//  C1-DatingApp
+//
+//  Created by Ramdan on 09/04/25.
+//
+
+import SwiftUI
+
+@main
+struct C1_DatingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
